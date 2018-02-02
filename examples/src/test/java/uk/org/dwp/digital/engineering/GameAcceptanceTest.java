7b73a9c5ebe0b82311dc941fe0709d4a718d1249
julian.brown <julian.brown1@dwp.gsi.gov.uk>
2018-02-02 15:01:59 +0000

    All three example games are recorded as black box acceptance tests

--- examples/src/test/java/uk/org/dwp/digital/engineering/GameAcceptanceTest.java
+++ examples/src/test/java/uk/org/dwp/digital/engineering/GameAcceptanceTest.java
@@ -1,6 +1,7 @@
 package uk.org.dwp.digital.engineering;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import uk.org.dwp.digital.engineering.tenpinbowlingkata.Game;
 
@@ -8,9 +9,13 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
 public class GameAcceptanceTest {
+    private Game game;
+    @Before
+    public void setup() {
+        game = new Game();
+    }
     @Test
     public void exampleScoreCard() {
-        Game game = new Game();
         game.roll(1);
         game.roll(4);
         game.roll(4);
@@ -31,5 +36,45 @@ public class GameAcceptanceTest {
         game.roll(8);
         game.roll(6);
         Assert.assertThat("Expected a score of 133", game.score(), is(equalTo(133)));
+    }
+
+    @Test
+    public void secondExampleScoreCard() {
+        game.roll(7);
+        game.roll(2);
+        game.roll(8);
+        game.roll(2);
+        game.roll(10);
+        game.roll(9);
+        game.roll(1);
+        game.roll(6);
+        game.roll(2);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(0);
+        game.roll(8);
+        game.roll(9);
+        game.roll(1);
+        game.roll(8);
+        Assert.assertThat("Expected a score of 167", game.score(), is(equalTo(167)));
+    }
+
+    @Test
+    public void perfectGameExampleScoreCard()
+    {
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        game.roll(10);
+        Assert.assertThat("Expected a score of 300", game.score(), is(equalTo(300)));
     }
 }
