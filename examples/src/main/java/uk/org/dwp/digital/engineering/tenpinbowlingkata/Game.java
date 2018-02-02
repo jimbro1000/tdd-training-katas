0128045df9940017fa4d5770b1316943194b1a2e
julian.brown <julian.brown1@dwp.gsi.gov.uk>
2018-02-02 11:01:18 +0000

    Refactored magic numbers in score calculation

--- examples/src/main/java/uk/org/dwp/digital/engineering/tenpinbowlingkata/Game.java
+++ examples/src/main/java/uk/org/dwp/digital/engineering/tenpinbowlingkata/Game.java
@@ -7,6 +7,8 @@ public class Game {
     private static final int MINIMUM_PINS = 0;
     private static final int MAXIMUM_PINS = 10;
     private static final int INCOMPLETE_GAME_SCORE = -1;
+    private static final int PINS_IN_FRAME = 10;
+    private static final int FRAMES_IN_GAME = 10;
     List<Integer> rollResult = new ArrayList<>();
 
     public void roll(int pins) {
@@ -19,19 +21,19 @@ public class Game {
         int final_score = INCOMPLETE_GAME_SCORE;
         int framesCompleted = 0;
         int frame = 1;
-        int pinsLeftInFrame = 10;
+        int pinsLeftInFrame = PINS_IN_FRAME;
         int cumulative_score = 0;
         for (Integer pins : rollResult) {
-            if (pinsLeftInFrame < 10) {
+            if (pinsLeftInFrame < PINS_IN_FRAME) {
                 framesCompleted = frame;
                 ++frame;
-                pinsLeftInFrame = 10;
+                pinsLeftInFrame = PINS_IN_FRAME;
             } else {
                 pinsLeftInFrame -= pins;
             }
             cumulative_score += pins;
         }
-        if (framesCompleted == 10) {
+        if (framesCompleted == FRAMES_IN_GAME) {
             final_score = cumulative_score;
         }
         return final_score;
