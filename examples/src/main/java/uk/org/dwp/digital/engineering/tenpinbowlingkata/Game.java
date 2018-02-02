b2d5650c4ac3862b85a93d220f1f013964d6bdb6
julian.brown <julian.brown1@dwp.gsi.gov.uk>
2018-02-02 10:37:51 +0000

    Refactored magic number for maximum number of accepted pins

--- examples/src/main/java/uk/org/dwp/digital/engineering/tenpinbowlingkata/Game.java
+++ examples/src/main/java/uk/org/dwp/digital/engineering/tenpinbowlingkata/Game.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class Game {
     private static final int MINIMUM_PINS = 0;
+    private static final int MAXIMUM_PINS = 10;
     List<Integer> rollResult = new ArrayList<>();
 
     public void roll(int pins) {
-        if (pins >= MINIMUM_PINS && pins <= 10) {
+        if (pins >= MINIMUM_PINS && pins <= MAXIMUM_PINS) {
             rollResult.add(pins);
         }
     }
