34e8547e20f76753a2087182b7c8b4301eedb19f
julian.brown <julian.brown1@dwp.gsi.gov.uk>
2018-02-02 10:30:37 +0000

    refactored tests to remove duplication of game instantiation

--- examples/src/test/java/uk/org/dwp/digital/engineering/tenpinbowlingkata/GameTest.java
+++ examples/src/test/java/uk/org/dwp/digital/engineering/tenpinbowlingkata/GameTest.java
@@ -1,22 +1,27 @@
 package uk.org.dwp.digital.engineering.tenpinbowlingkata;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
 public class GameTest {
+    private Game game;
+    @Before
+    public void setup() {
+        game = new Game();
+    }
+
     @Test
     public void methodRollAcceptsPinsAsAnIntegerAndStoresTheValue() {
-        Game game = new Game();
         game.roll(1);
         Assert.assertThat("Expected roll to store a value of 1", game.rollResult.get(0), is(equalTo(1)));
     }
 
     @Test
     public void methodRollStoresPinsAsASequenceOfValues() {
-        Game game = new Game();
         game.roll(2);
         game.roll(8);
         Assert.assertThat("Expected roll to store a value of 2 for the first roll", game.rollResult.get(0), is(equalTo(2)));
